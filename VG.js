d5c4608a27c78a5bada6c1b2a5d16aac8db96aca
laurasupinski <61475530+laurasupinski@users.noreply.github.com>
2020-03-04 13:33:30 -0600

    Lines 51-75 for js

--- VG.js
+++ VG.js
@@ -1,1 +1,26 @@
 hi
+// Lines 51- 75
+cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update(){
+    if (cursors.left.isDown) {
+      player.setVelocityX(-100); // we want to apply a negative x velocity to go left on the screen
+    } else if (cursors.right.isDown) {
+      player.setVelocityX(100); // we want to apply a positive x velocity to go right on the screen
+    } else {   
+      //if we don't include this then the player will always be going right or left instead of a L/R neutral upwards direction
+      player.setVelocityX(0);
+    }
+
+    if (player.body.touching.down) {
+      player.setVelocityY(-250);
+    }
+
+
+    // We want to the platforms to move downwards and create an effect of progressing upwards
+    // This can be done by moving the camera but can also be done by moving the world around the player
+    if (player.body.y <  gameOptions.height/2) {
+      platforms.children.iterate(updateY, this);
+    }
+  }
